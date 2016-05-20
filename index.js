50dd155ed1c14a091149bd27073c877ef0279c3f
fdsilva <neitonfsilva@gmail.com>
2016-05-20 16:22:56 -0300

    Adding tasks object as a static resource

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var espress = require('espress');
-//import express from "express";
-const PORT  = 3000;
+//var espress = require('espress');
+import express from "express";
 
+const PORT  = 3000;
 const app = express();
 
 app.get("/", (req, res) => res.json({starus: "Task_MAneger_API"}));
--- task_maneger_api/index.js
+++ task_maneger_api/index.js
@@ -6,4 +6,13 @@ const app = express();
 
 app.get("/", (req, res) => res.json({starus: "Task_MAneger_API"}));
 
-app.listen(PORT, () => console.log('Task_Maneger_API -Port ${PORT}'));
+app.get("/tasks", (req, res) =>{
+	res.json({
+		tasks: [
+		{title: "By a car"},
+		{title: "feed the cat"},
+		]
+	});
+});
+
+app.listen(PORT, () => console.log(`Task_Maneger_API -Port ${PORT}`));
